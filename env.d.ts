4d902b89c108211398aa8d9778dfb16c7dccec32
Ray <1497161283@qq.com>
2025-01-07 09:35:44 +0800

    chore(init): 定义vue模块类型

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+/** 定义vue模块类型 */
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent
+  export default component
+}
