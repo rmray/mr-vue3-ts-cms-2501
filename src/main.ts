dfe942203638bece441129c31417f69e3a128691
Ray <1497161283@qq.com>
2025-01-07 10:58:48 +0800

    chore(init): 工程化规范-配置editorconfig、prettier、eslint

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import 'normalize.css'
 import './assets/main.less'
 
 import { createApp } from 'vue'
+
 import App from './App.vue'
+
+let name = 'test'
+
+// test.name
 
 createApp(App).mount('#app')
